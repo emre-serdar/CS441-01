bef44735d2c5b0155f2cac02fa98913f49714e7c
emre-serdar <eserdar1@binghamton.edu>
2021-09-13 11:58:51 -0400

    HelloWorld app is done

--- HelloApp/app/src/main/java/com/example/helloapp/MainActivity.kt
+++ HelloApp/app/src/main/java/com/example/helloapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.helloapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener{toast("Hello All The World!")}
     }
     // comment block
 }
